908f82acc18eaf63b77518dd88181d4a9de35614
3wlh <1094890624@qq.com>
2025-07-17 16:50:33 +0800

    up

--- diy_make/armsr/make1.sh
+++ diy_make/armsr/make1.sh
@@ -68,26 +68,71 @@ echo "================================================================="
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建镜像..."
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 系统Version: ${VERSION} ..."
 #========== 定义所需安装的包列表 ==========#
-PACKAGES="\
-    acpid attr base-files bash bc blkid block-mount blockd bsdtar btrfs-progs busybox bzip2 \
-    cgi-io chattr comgt comgt-ncm containerd coremark coreutils coreutils-base64 coreutils-nohup \
-    coreutils-truncate curl docker docker-compose dockerd dosfstools dumpe2fs e2freefrag e2fsprogs \
-    exfat-mkfs f2fs-tools f2fsck fdisk gawk getopt git gzip hostapd-common iconv iw iwinfo jq \
-    jshn kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script liblucihttp \
-    liblucihttp-lua losetup lsattr lsblk lscpu mkf2fs mount-utils openssl-util parted \
-    perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 \
-    pigz ppp ppp-mod-pppoe pv rename resize2fs runc tar tini ttyd tune2fs \
-    uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch uuidgen wget-ssl whereis \
-    which wpad-basic wwan xfs-fsck xfs-mkfs xz xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
-    \
-    luci luci-base luci-compat luci-i18n-base-zh-cn luci-lib-base luci-lib-docker \
-    luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
-    luci-mod-status luci-mod-system luci-proto-3g luci-proto-ipip luci-proto-ipv6 \
-    luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay \
-    \
-    luci-app-amlogic luci-i18n-amlogic-zh-cn \
-    \
-    "
+PACKAGES=""
+#========== 删除插件包 ==========#
+PACKAGES="$PACKAGES -luci-app-cpufreq"
+if [[ "${BRANCH}" == "openwrt" ]]; then
+PACKAGES="$PACKAGES -dnsmasq"
+fi
+#========== 添加内核驱动 ==========#
+PACKAGES="$PACKAGES kmod-tcp-bbr kmod-lib-zstd kmod-thermal kmod-input-core kmod-gpio-cascade" # kmod-input-core kmod-thermal
+PACKAGES="$PACKAGES kmod-drm kmod-drm-buddy kmod-drm-display-helper kmod-drm-kms-helper kmod-drm-mipi-dbi kmod-drm-ttm"
+PACKAGES="$PACKAGES usb-modeswitch kmod-usb-core kmod-usb2 kmod-usb3 kmod-usb-ohci kmod-usb-storage kmod-scsi-generic" # USB驱动
+if [[ "${BRANCH}" == "immortalwrt" ]]; then
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 添加${BRANCH}内核模块..."
+PACKAGES="$PACKAGES kmod-drm-gem-shmem-helper kmod-drm-dma-helper"
+PACKAGES="$PACKAGES kmod-nft-offload kmod-nft-fullcone kmod-nft-nat"
+else
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 添加${BRANCH}内核模块..."
+PACKAGES="$PACKAGES kmod-drm-dma-helper"
+PACKAGES="$PACKAGES kmod-nft-offload kmod-nft-nat"
+PACKAGES="$PACKAGES luci-lib-ipkg"
+fi
+if [[ "$(echo ${VERSION} |  cut -d '.' -f 1 )" -ge "24" ]]; then
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+else
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+fi
+#========== 添加插件包 ==========#
+PACKAGES="$PACKAGES hub-ctrl uhubctl"
+PACKAGES="$PACKAGES busybox uci luci uhttpd opkg curl openssl-util ds-lite e2fsprogs lsblk resolveip swconfig zram-swap"
+PACKAGES="$PACKAGES bash luci-base nano wget-ssl openssh-sftp-server coremark htop"
+
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-base-zh-cn" 
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ramfree-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+
+if $SERVICE; then
+PACKAGES="$PACKAGES docker dockerd docker-compose luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn luci-i18n-hd-idle-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-cloudflared-zh-cn"
+PACKAGES="$PACKAGES luci-app-store"
+PACKAGES="$PACKAGES luci-i18n-openlist-zh-cn"
+PACKAGES="$PACKAGES luci-app-unishare"
+PACKAGES="$PACKAGES luci-app-sunpanel"
+else
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+# PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-app-v2ray-server"
+PACKAGES="$PACKAGES nikki luci-app-nikki luci-i18n-nikki-zh-cn"
+# DDNS解析
+PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn ddns-scripts_aliyun ddns-scripts-cloudflare ddns-scripts-dnspod bind-host" #knot-host drill bind-host
+# 增加几个必备组件 方便用户安装iStore
+# PACKAGES="$PACKAGES fdisk"
+# PACKAGES="$PACKAGES script-utils"
+# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 添加Docker插件
+if $INCLUDE_DOCKER; then
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 添加docker插件..." 
+PACKAGES="$PACKAGES docker dockerd docker-compose luci-i18n-dockerman-zh-cn"
+fi
+fi
+
 #=============== 开始打包镜像 ===============#
 echo "============================= 默认插件 ============================="
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 默认插件包："
