2c639ab57102bb32985a18066bc3e09aaa3c623f
3wlh <1094890624@qq.com>
2025-07-24 22:28:06 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -13,12 +13,12 @@ while IFS= read -r LINE; do
     md5name=$(find "$(pwd)/dl" -type f -name "${name}" 2>/dev/null -exec md5sum -b {} \; | awk '{print $1}')
     echo "插件${name}:{\"md5url\": \"${md5url}\",\"md5name\": \"${md5name}\"}"
     if [[ "${md5url}" == "${md5name}" ]]; then
-        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m -【${name}】无更新插件.\e[0m"
+        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m -【${name}】无更新插件.\e[0m\n"
     else
         # 删除 GitHub 缓存
         rm -rf "$(pwd)/dl/*"
         echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m -【${name}】有更新插件.\e[0m"
-        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 删除所有缓存插件！\e[0m"
+        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 删除所有缓存插件！\e[0m\n"
         echo "cache=delete" >> "$(pwd)/bin/.bashrc"
         break
     fi
