57d254823181f6985216b39dad2fe0d5f630fdd3
3wlh <1094890624@qq.com>
2025-07-24 23:01:27 +0800

    Update Kmods.sh

--- .github/.sh/Kmods.sh
+++ .github/.sh/Kmods.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-Kernel=$(find "$(pwd)/build_dir/" -type f -name "kernel_*.ipk" 2>/dev/null)
-[[ -z "${Kernel}" ]] && Kernel=$(find "$(pwd)/build_dir/" -type f -name "kernel_*.apk" 2>/dev/null)
+Kernel=$(find "$(pwd)/build_dir/" -type f -name "kernel_*.[ia]pk" 2>/dev/null -exec basename {} \;)
 [[ -n "${Kernel}" ]] && echo "$(date '+%Y-%m-%d %H:%M:%S') - Kernel版本：$(basename ${Kernel})"
 cat "$(pwd)/repositories.conf" | \
 while IFS= read -r LINE; do
@@ -10,14 +9,4 @@ while IFS= read -r LINE; do
 	kmods_url="${url%/*}"
 	kmods_version="${url##*/}"
 	echo "$(date '+%Y-%m-%d %H:%M:%S') - kmods版本：${kmods_version}"
-	# wget -qO- "${kmods_url}" | \
-	# while IFS= read -r line; do
-    	# if [[ "$line" == *"${1}"* ]]; then
-			# FILE=$(echo "$line" | grep -Eo 'href="[^"]*' | sed 's/href="//' | tr -d "/")
-			# if [[ -n "$line" ]]; then
-				# sed -i "s/${kmods_version}/${FILE}/" "$(pwd)/repositories.conf"
-				# echo "$(date '+%Y-%m-%d %H:%M:%S') - 修改kmods版本：${FILE}"
-			# fi
-		# fi
-	# done
 done
