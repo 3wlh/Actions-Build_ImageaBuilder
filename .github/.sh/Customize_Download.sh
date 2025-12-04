a0c4c306eb91dd027abfd082f8d2f9db109a1ff5
3wlh <1094890624@qq.com>
2025-12-04 21:55:57 +0800

    Update Customize_Download.sh

--- .github/.sh/Customize_Download.sh
+++ .github/.sh/Customize_Download.sh
@@ -7,6 +7,9 @@ function OECT() {
 # 下载文件
 sub_cgi="https://github.com/3wlh/Actions-Build_Source/releases/download/sub.cgi/sub.cgi-aarch64_generic"
 curl -# -L --fail "${sub_cgi}" -o "$(pwd)/files/www/cgi-bin/sub.cgi" && chmod 755 "$(pwd)/files/www/cgi-bin/sub.cgi"
+page_cgi="https://github.com/3wlh/Actions-Build_Source/releases/download/page.cgi/page.cgi-aarch64_generic"
+curl -# -L --fail "${page_cgi}" -o "$(pwd)/files/www/cgi-bin/page.cgi" && chmod 755 "$(pwd)/files/www/cgi-bin/page.cgi
+"
 }
 function 5Plus() {
 echo "5Plus"
