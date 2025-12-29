7f385fb1ee4320b44988910c4cfb0a36f7e716c0
3wlh <1094890624@qq.com>
2025-12-30 04:00:21 +0800

    Update 99-custom.sh

--- diy_files/armsr/wxy-oect/99-custom.sh
+++ diy_files/armsr/wxy-oect/99-custom.sh
@@ -132,8 +132,8 @@ uci set system.@system[0].hostname="${settings_model}"
 fi
 uci commit system
 fi
-uci set netconfig.@general[].config_url="http://3wlh.github.io/Script/OpenWrt/Config_sh/OECT.sh"
-uci commit netconfig
+uci set runscript.@general[].script_url="http://3wlh.github.io/Script/OpenWrt/Config_sh/OECT.sh"
+uci commit runscript
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
 NEW_DESCRIPTION="Compiled by 3wlh"
