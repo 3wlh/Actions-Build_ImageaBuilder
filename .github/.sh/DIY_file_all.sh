e0c3fb3a006325533818d4b9c9c2786f8604751f
3wlh <1094890624@qq.com>
2026-01-08 17:15:00 +0800

    up

--- .github/.sh/DIY_file_all.sh
+++ .github/.sh/DIY_file_all.sh
@@ -10,3 +10,8 @@ mv -f "$(pwd)/all/sys-opkg.sh" "$(pwd)/files/etc/uci-defaults"
 [[ -d "$(pwd)/files/etc/profile.d" ]] || mkdir -p "$(pwd)/files/etc/profile.d"
 [[ -f "$(pwd)/all/sys-sysinfo.sh" ]] && \
 mv -f "$(pwd)/all/sys-sysinfo.sh" "$(pwd)/files/etc/profile.d"
+
+[[ -d "$(pwd)/files/www/luci-static/resources/view/status/include" ]] || \
+mkdir -p "$(pwd)/www/luci-static/resources/view/status/include"
+[[ -f "$(pwd)/all/port.js" ]] && \
+mv -f "$(pwd)/all/port.js" "$(pwd)/files/luci-static/resources/view/status/include/29_port.js"
